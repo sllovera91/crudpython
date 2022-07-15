c2d2ce8a08415f00e95433b3017f2f64d77a5e6c
Santiago Llovera <105535098+sllovera91@users.noreply.github.com>
2022-07-15 14:32:04 -0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+if (document.getElementById("app")) {
+    const app = new Vue({
+        el: "#app",
+        data: {
+            productos: [],
+            errored: false,
+            loading: true
+        },
+        created() {
+            var url = 'http://localhost:5000/productos'
+            this.fetchData(url)
+        },
+        methods: {
+            fetchData(url) {
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        this.productos = data;
+                        this.loading = false;
+                    })
+                    .catch(err => {
+                        this.errored = true
+                    })
+            },
+            eliminar(producto) {
+                const url = 'http://localhost:5000/productos/' + producto;
+                var options = {
+                    method: 'DELETE',
+                }
+                fetch(url, options)
+                    .then(res => res.text()) // or res.json()
+                    .then(res => {
+                        location.reload();
+                    })
+            }
+        }
+    })
+}
